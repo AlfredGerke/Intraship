55d1c13ee0b28ae038c25ef8b847689ed89de72f
AlfredGerke <alfred.gerke@software-aus-erwitte.de>
2014-06-29 12:44:12 +0200

    Anpassungen laut Issue #5
    * Zwischenstand

--- webapproot/pages/MainTablet/MainTablet.widgets.js
+++ webapproot/pages/MainTablet/MainTablet.widgets.js
@@ -1,24 +1,35 @@
 MainTablet.widgets = {
-	varNavItems: ["wm.Variable", {"isList":true,"json":"[{\"dataValue\":\"CreateShipmentDD\"},{\"dataValue\":\"CreateShipmentTD\"},{\"dataValue\":\"GetShipmentDD\"},{\"dataValue\":\"DeleteShipmentDD\"},{\"dataValue\":\"DeleteShipmentTD\"}]","type":"StringData"}, {}],
-	navigationCall1: ["wm.NavigationCall", {}, {}, {
-		input: ["wm.ServiceInput", {"type":"gotoLayerInputs"}, {}]
+	varNavItems: ["wm.Variable", {"isList":true,"json":"[{\"dataValue\":\"CreateShipmentDD\"},{\"dataValue\":\"GetShipmentDD\"},{\"dataValue\":\"DeleteShipmentDD\"}]","type":"StringData"}, {}],
+	navCallInformation: ["wm.NavigationCall", {"operation":"gotoPageContainerPage"}, {}, {
+		input: ["wm.ServiceInput", {"type":"gotoPageContainerPageInputs"}, {}, {
+			binding: ["wm.Binding", {}, {}, {
+				wire: ["wm.Wire", {"expression":"\"Information\"","targetProperty":"pageName"}, {}],
+				wire1: ["wm.Wire", {"expression":undefined,"source":"pagContainer","targetProperty":"pageContainer"}, {}]
+			}]
+		}]
 	}],
-	lbxMain: ["wm.Layout", {"horizontalAlign":"left","verticalAlign":"top"}, {}, {
-		pnlDesktop: ["wm.Panel", {"height":"100%","horizontalAlign":"left","layoutKind":"left-to-right","verticalAlign":"top","width":"100%"}, {}, {
-			pnlLeft: ["wm.Panel", {"height":"100%","horizontalAlign":"left","verticalAlign":"top","width":"270px"}, {}, {
-				lstNavigation: ["wm.List", {"_classes":{"domNode":["GridListStyle","MobileListStyle"]},"columns":[
+	lbxMain: ["wm.Layout", {"horizontalAlign":"left","styles":{},"verticalAlign":"top"}, {}, {
+		pnlDesktop: ["wm.Panel", {"deviceType":["tablet"],"height":"100%","horizontalAlign":"left","layoutKind":"left-to-right","verticalAlign":"top","width":"100%"}, {}, {
+			pnlLeft: ["wm.Panel", {"deviceType":["tablet"],"height":"100%","horizontalAlign":"left","verticalAlign":"top","width":"270px"}, {}, {
+				lstNavigation: ["wm.List", {"_classes":{"domNode":["GridListStyle","MobileListStyle"]},"border":"1","columns":[
 {"show":true,"field":"dataValue","title":"Geschäftskundenversand API","width":"100%","align":"left","formatFunc":"","mobileColumn":false},
 {"show":false,"field":"PHONE COLUMN","title":"-","width":"100%","align":"left","expression":"\"<div class='MobileRowTitle'>\" +\n\"Geschäftskundenversand API: \" + ${dataValue} +\n\"</div>\"\n\n","mobileColumn":true},
 {"show":true,"controller":"rightarrow","width":"20px","title":"-","field":"_rightArrow","mobileColumn":true}
-],"deviceType":["tablet"],"height":"100%","isNavigationMenu":true,"minDesktopHeight":60,"primaryKeyFields":["dataValue"],"rightNavArrow":true,"styleAsGrid":false}, {"onSelect":"lstNavigationSelect"}, {
+],"deviceType":["tablet"],"height":"100%","isNavigationMenu":true,"minDesktopHeight":60,"primaryKeyFields":["dataValue"],"rightNavArrow":true,"scrollToSelection":true,"styleAsGrid":false,"styles":{}}, {"onSelect":"lstNavigationSelect"}, {
 					binding: ["wm.Binding", {}, {}, {
 						wire: ["wm.Wire", {"expression":undefined,"source":"varNavItems","targetProperty":"dataSet"}, {}],
 						wire1: ["wm.Wire", {"expression":undefined,"source":"varNavItems.queryVar","targetProperty":"selectedItem"}, {}]
 					}]
 				}]
 			}],
-			pnlDetail: ["wm.Panel", {"height":"100%","horizontalAlign":"left","verticalAlign":"top","width":"100%"}, {}, {
-				pagContainer: ["wm.PageContainer", {"deferLoad":true}, {}]
+			pnlDetail: ["wm.Panel", {"deviceType":["tablet"],"height":"100%","horizontalAlign":"left","verticalAlign":"top","width":"100%"}, {}, {
+				pnlDetailTop: ["wm.Panel", {"height":"35px","horizontalAlign":"left","layoutKind":"left-to-right","verticalAlign":"top","width":"100%"}, {}, {
+					edtShipmentNr: ["wm.Text", {"caption":"ShipmentNr","dataValue":"","displayValue":"","emptyValue":"emptyString","height":"100%","helpText":undefined,"placeHolder":"Number for DD-Services","width":"343px"}, {}]
+				}],
+				pagContainer: ["wm.PageContainer", {"deferLoad":true,"deviceType":["tablet"],"styles":{}}, {}],
+				pnlDetailButtom: ["wm.Panel", {"height":"40px","horizontalAlign":"center","layoutKind":"left-to-right","verticalAlign":"top","width":"100%"}, {}, {
+					btnInfo: ["wm.Button", {"caption":"About","height":"100%","margin":"4"}, {"onclick":"navCallInformation"}]
+				}]
 			}]
 		}]
 	}]
